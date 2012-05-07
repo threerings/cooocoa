62a082f5c92c11f3be98018bf346501ebfa1897a
Tim Conkling <tconkling@gmail.com>
2012-05-06 17:01:38 -0700

    xcode project

---
+++ Tests/cooocoaTests.h
@@ -0,0 +1,13 @@
+//
+//  cooocoaTests.h
+//  cooocoaTests
+//
+//  Created by Tim Conkling on 5/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface cooocoaTests : SenTestCase
+
+@end
---
+++ Tests/cooocoaTests.m
@@ -0,0 +1,32 @@
+//
+//  cooocoaTests.m
+//  cooocoaTests
+//
+//  Created by Tim Conkling on 5/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "cooocoaTests.h"
+
+@implementation cooocoaTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in cooocoaTests");
+}
+
+@end
