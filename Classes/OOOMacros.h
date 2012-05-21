c59bbb4c5e95e4891e61e4a9dace7d8bf7ca9fe6
Tim Conkling <tconkling@gmail.com>
2012-05-21 14:06:24 -0700

    OOO_STATIC_OBJECT macro

--- Classes/OOOMacros.h
+++ Classes/OOOMacros.h
@@ -1,6 +1,8 @@
 //
 // cooocoa - Copyright 2012 Three Rings Design
 
+/// Returns an NSMutableArray containing a method's varargs
+/// -(NSArray*)toArray:(Foo*)obj1, ... { return OOO_VARARGS_TO_ARRAY(Foo*, obj1); }
 #define OOO_VARARGS_TO_ARRAY(type, first) ({ \
     va_list ap; \
     va_start(ap, child); \
@@ -8,4 +10,12 @@
     for (; child != nil; child = va_arg(ap, type)) [children addObject:child]; \
     va_end(ap); \
     children; \
-    })
+})
+
+/// Creates and initializes a static object.
+/// -(Foo*)getSingletonFoo { return OOO_STATIC_OBJECT([[Foo alloc] init]); }
+#define OOO_STATIC_OBJECT(INIT_FUNC) ({ \
+    static id gObject = nil; \
+    @synchronized(self) { if (gObject == nil) gObject = INIT_FUNC;  } \
+    gObject; \
+})
