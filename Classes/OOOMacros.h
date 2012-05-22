f28645712e886287647b6a19cbc7dd3172151728
Tim Conkling <tconkling@gmail.com>
2012-05-21 17:45:13 -0700

    a fast AND threadsafe singleton initializer

--- Classes/OOOMacros.h
+++ Classes/OOOMacros.h
@@ -14,21 +14,11 @@
 
 /// Creates and initializes a static object.
 /// -(Foo*)getSingletonFoo { return OOO_STATIC_OBJECT([[Foo alloc] init]); }
-
-#ifdef OOO_THREAD_SAFE
-
-    #define OOO_STATIC_OBJECT(INIT_FUNC) ({ \
-        static id gObject = nil; \
-        @synchronized(self) { if (gObject == nil) gObject = INIT_FUNC;  } \
-        gObject; \
-    })
-
-#else
-
-    #define OOO_STATIC_OBJECT(INIT_FUNC) ({ \
-        static id gObject = nil; \
-        if (gObject == nil) gObject = INIT_FUNC; \
-        gObject; \
-    })
+#define OOO_STATIC_OBJECT(INIT_FUNC) ({ \
+    static dispatch_once_t once; \
+    static id gObject = nil; \
+    dispatch_once(&once, ^{ gObject = INIT_FUNC; }); \
+    gObject; \
+})
 
 #endif
