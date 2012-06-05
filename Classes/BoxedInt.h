035497bdc1d8ac285e59f4b9b16bdcf739035192
Tim Conkling <tconkling@gmail.com>
2012-06-05 13:56:07 -0700

    Where did these come from?

--- Classes/BoxedInt.h
+++
@@ -1,6 +0,0 @@
-//
-// cooocoa - Copyright 2012 Three Rings Design
-
-@interface BoxedInt : NSObject
-@property int value;
-@end
--- Classes/BoxedInt.m
+++
@@ -1,8 +0,0 @@
-//
-// cooocoa - Copyright 2012 __MyCompanyName__
-
-#import "BoxedInt.h"
-
-@implementation BoxedInt
-
-@end
