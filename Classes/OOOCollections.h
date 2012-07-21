1e2c36741945350bc81fc8c26f6255e9ff07e6f3
Tim Conkling <tconkling@gmail.com>
2012-07-20 18:42:34 -0700

    OOOCollections changes
    - OOOCollections.allObjects
    - documentation & formatting
    - don't add nil objects to the array returned by filter

--- Classes/OOOCollections.h
+++ Classes/OOOCollections.h
@@ -5,9 +5,21 @@
 
 @interface OOOCollections : NSObject
 
-+ (NSMutableArray*)filter:(id<NSFastEnumeration>)collection pred:(BOOL (^)(id object))pred;
-+ (NSMutableArray*)map:(id<NSFastEnumeration>)collection transformer:(id (^)(id object))transformer;
-+ (id)findObject:(id<NSFastEnumeration>)collection pred:(BOOL (^)(id object))pred;
+/// Returns an array containing all objects in the collection 
+/// (nil objects will be converted to NSNull)
++ (NSMutableArray*)allObjects:(id<NSFastEnumeration>)collection;
+
+/// Returns an array containing all objects in the collection for which 'pred' returns YES
+/// (nil objects will be converted to NSNull)
++ (NSMutableArray*)filter:(id<NSFastEnumeration>)collection pred:(BOOL(^)(id object))pred;
+
+/// Returns an array containing the given transform applied to each object in the collection
+/// (nil objects will be converted to NSNull)
++ (NSMutableArray*)map:(id<NSFastEnumeration>)collection transformer:(id(^)(id object))transformer;
+
+/// Returns the first object in the collection for which 'pred' returns YES,
+/// or nil if no such object exists.
++ (id)findObject:(id<NSFastEnumeration>)collection pred:(BOOL(^)(id object))pred;
 
 /// Returns an NSEnumerator that will enumerate over a single object
 + (id<NSFastEnumeration>)singleton:(id)object;
--- Classes/OOOCollections.m
+++ Classes/OOOCollections.m
@@ -6,25 +6,33 @@
 
 @implementation OOOCollections
 
-+ (NSMutableArray*)filter:(id<NSFastEnumeration>)collection pred:(BOOL (^)(id))pred {
-    NSMutableArray *array = [[NSMutableArray alloc] init];
++ (NSMutableArray*)allObjects:(id<NSFastEnumeration>)collection {
+    NSMutableArray* array = [[NSMutableArray alloc] init];
+    for (id obj in collection) {
+        [array addObject:OOONilToNSNull(obj)];
+    }
+    return array;
+}
+
++ (NSMutableArray*)filter:(id<NSFastEnumeration>)collection pred:(BOOL(^)(id))pred {
+    NSMutableArray* array = [[NSMutableArray alloc] init];
     for (id obj in collection) {
         if (pred(obj)) {
-            [array addObject:obj];
+            [array addObject:OOONilToNSNull(obj)];
         }
     }
     return array;
 }
 
-+ (NSMutableArray*)map:(id<NSFastEnumeration>)collection transformer:(id (^)(id))transformer {
-    NSMutableArray *array = [[NSMutableArray alloc] init];
++ (NSMutableArray*)map:(id<NSFastEnumeration>)collection transformer:(id(^)(id))transformer {
+    NSMutableArray* array = [[NSMutableArray alloc] init];
     for (id obj in collection) {
         [array addObject:OOONilToNSNull(transformer(obj))];
     }
     return array;
 }
 
-+ (id)findObject:(id<NSFastEnumeration>)collection pred:(BOOL (^)(id))pred {
++ (id)findObject:(id<NSFastEnumeration>)collection pred:(BOOL(^)(id))pred {
     for (id obj in collection) {
         if (pred(obj)) {
             return obj;
